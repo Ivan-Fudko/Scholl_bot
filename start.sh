6f6bef28e0743d0f6201b2d8d84bf21c0051aec0
Ivan-Fudko <79043864+Ivan-Fudko@users.noreply.github.com>
2023-05-01 19:40:33 +0500

    3

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Exit еар1у оп eppops
+set -еи
+# Python buffeps stdout. Without this, уои won't see what уои print “ in the Activity Logs
+expopt PYTHONUNBUFFERED=true
+# Insta11 Python З virtual env
+VIRTUALENV= . /venv
+if [! -d $VIRTUALENV ] ; then
+  python3 -m venv $VIRTUALENV
+fi
+# Insta11 pip into virtual enviponment
+if [ ! -f $VIRTUALENV/bin/pip ] ; then
+  curl --silent --show-error --retry 5 https://bootstpap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
+fi
+# Insta11 the requirements
+$VIRTUALENV/bin/pip install -г requirements.txt
+# Run уоиг glopious application
+$VIRTUALENV/bin/python3 server. ру
